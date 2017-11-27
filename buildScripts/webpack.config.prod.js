39796550ca2533653de331b425d65a042279226b
Peter <netbka@gmail.com>
2017-11-27 18:03:46 +0300

    BuildServer

--- buildScripts/webpack.config.prod.js
+++ buildScripts/webpack.config.prod.js
@@ -1,27 +1,49 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, '../src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, '../src/vendor'), 
+    main: path.resolve(__dirname, '../src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, '../dist'),
-	publicPath: '/',
-    filename: 'bundle.js'
+	  publicPath: '/',
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
     new webpack.LoaderOptionsPlugin({
         debug: true,
         noInfo: false,
-	  }),
-	  //Create HTML file and include reference to bundled JS
+    }),
+    //Generate an external css file with a hash
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    //Hash file using MD5 so their names change when content changes
+    new WebpackMd5Hash(),
+
+    //Use CommonsChunkPlugin to create seperate bunde  
+    //of vendor libs so they're cached separately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'  // same as in entry
+    }),
+    
+    //Create HTML file and include reference to bundled JS
 	  new HtmlWebpackPlugin({
-		  template: './src/index.html',
-		  inject:true
+      template: './src/index.html',
+      minify:{
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes:true,
+        minifyJS:true,
+        minifyCSS:true,
+      },
+      inject:true,
+      hash: true
 	  }),
 	  //remove duplications
 	  new webpack.optimize.DedupePlugin(),
@@ -32,7 +54,8 @@ export default {
   module: {
     rules: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+      //{test: /\.css$/, loaders: ['style-loader','css-loader']}
+      {test: /\.css$/, use: ExtractTextPlugin.extract({fallback: "style-loader",use: "css-loader"})}
     ]
   }
 };
---
+++ src/vendor.js
@@ -0,0 +1,8 @@
+/*
+This file will contain references to vendor libraries used in project.
+This is used by webpack for production build only.
+any files not referenced here will be bundled into main.js for the prod build
+*/
+/* eslint-disable no-unused-vars*/ 
+
+import fetch from 'whatwg-fetch';
